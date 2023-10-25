e534456a64fdb672732975544a41e8896cd4f343
Kanita Kadusic <kkadusic2@etf.unsa.ba>
2023-10-26 00:17:20 +0200

    User input

--- src/main/java/ba/unsa/etf/rpr/Main.java
+++ src/main/java/ba/unsa/etf/rpr/Main.java
@@ -1,7 +1,24 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        List<Double> nums = new ArrayList<>();
+
+        for (;;) {
+            System.out.print("enter number: ");
+            String input = scanner.nextLine();
+            if (input.equals("stop")) break;
+
+            try {
+                nums.add(Double.parseDouble(input));
+            } catch (NumberFormatException e) {
+                System.out.print(" > re-");
+            }
+        }
     }
 }
