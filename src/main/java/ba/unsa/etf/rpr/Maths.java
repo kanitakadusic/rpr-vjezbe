76652539ebb16af16db7aa5ef04e3bd45a178384
Kanita Kadusic <kkadusic2@etf.unsa.ba>
2023-10-26 12:05:56 +0200

    Maths class modification

--- src/main/java/ba/unsa/etf/rpr/Maths.java
+++ src/main/java/ba/unsa/etf/rpr/Maths.java
@@ -1,25 +1,7 @@
 package ba.unsa.etf.rpr;
 
 public class Maths {
-    private static double pow(double base, int exp) {
-        double result = 1;
-
-        for (int i = 0; i < exp; i++) {
-            result *= base;
-        }
-
-        return result;
-    }
-
     public static double sin(double x) {
-        /*
-        double result = 0;
-
-        for (int i = 0; i <= 5; i++) {
-            result += Maths.pow(-1, i) * Maths.pow(x, 2 * i + 1) / Maths.fact(2 * i + 1);
-        }
-        */
-
         return java.lang.Math.sin(x);
     }
 
