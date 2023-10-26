1e8dc489decbbd995eaba733371e537601436c56
Kanita Kadusic <kkadusic2@etf.unsa.ba>
2023-10-26 13:22:36 +0200

    Get data from args

--- src/main/java/ba/unsa/etf/rpr/Main.java
+++ src/main/java/ba/unsa/etf/rpr/Main.java
@@ -2,7 +2,9 @@ package ba.unsa.etf.rpr;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Maths.sin(5));
-        System.out.println(Maths.fact(5));
+        for (String arg : args) {
+            System.out.println(Maths.sin(Double.parseDouble(arg)));
+            System.out.println(Maths.fact(Integer.parseInt(arg)));
+        }
     }
 }
