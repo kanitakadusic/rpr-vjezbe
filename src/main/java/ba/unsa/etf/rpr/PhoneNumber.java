7f51245d78c9dc93477bae653e582b942a0720eb
Kanita Kadusic <kkadusic2@etf.unsa.ba>
2023-10-31 07:05:07 +0100

    PhoneNumber constructor, getters and setters

--- src/main/java/ba/unsa/etf/rpr/PhoneNumber.java
+++ src/main/java/ba/unsa/etf/rpr/PhoneNumber.java
@@ -1,6 +1,20 @@
 package ba.unsa.etf.rpr;
 
 abstract public class PhoneNumber {
+    private String phoneNumber;
+
+    public PhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
     abstract public String print();
 
     @Override
