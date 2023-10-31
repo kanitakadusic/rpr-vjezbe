e82b676d872affc2c7501d247c410651c739329d
Kanita Kadusic <kkadusic2@etf.unsa.ba>
2023-10-31 13:57:43 +0100

    InternationalPhoneNumber implementation

--- src/main/java/ba/unsa/etf/rpr/InternationalPhoneNumber.java
+++ src/main/java/ba/unsa/etf/rpr/InternationalPhoneNumber.java
@@ -1,4 +1,28 @@
 package ba.unsa.etf.rpr;
 
-public class InternationalPhoneNumber {
+public class InternationalPhoneNumber extends PhoneNumber {
+    private String state;
+
+    public InternationalPhoneNumber(String state, String phoneNumber) {
+        super(phoneNumber);
+        this.state = state;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return this.getState() + ".." + this.getPhoneNumber();
+    }
+
+    @Override
+    public int hashCode() {
+        return this.state.hashCode();
+    }
 }
