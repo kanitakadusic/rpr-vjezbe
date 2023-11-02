e3b4dfcd831c11c140fadd511ec3f3f8f80fa443
Kanita Kadusic <kkadusic2@etf.unsa.ba>
2023-11-02 13:28:50 +0100

    Create custom exception

---
+++ src/main/java/ba/unsa/etf/rpr/InvalidParametersException.java
@@ -0,0 +1,7 @@
+package ba.unsa.etf.rpr;
+
+public class InvalidParametersException extends Exception {
+    public InvalidParametersException(String errorMessage) {
+        super(errorMessage);
+    }
+}
