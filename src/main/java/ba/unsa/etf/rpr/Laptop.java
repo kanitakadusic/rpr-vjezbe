4870c98934a00af9613b9d3fc3f3029bcdb7bd92
Kanita Kadusic <kkadusic2@etf.unsa.ba>
2023-11-09 12:52:59 +0100

    Laptop class implementation

---
+++ src/main/java/ba/unsa/etf/rpr/Laptop.java
@@ -0,0 +1,81 @@
+package ba.unsa.etf.rpr;
+
+public class Laptop {
+    private String brand, model, cpu, gpu;
+    private double price, size;
+    private int ram, hdd, ssd;
+
+    public Laptop() {}
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCpu() {
+        return cpu;
+    }
+
+    public void setCpu(String cpu) {
+        this.cpu = cpu;
+    }
+
+    public String getGpu() {
+        return gpu;
+    }
+
+    public void setGpu(String gpu) {
+        this.gpu = gpu;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public int getHdd() {
+        return hdd;
+    }
+
+    public void setHdd(int hdd) {
+        this.hdd = hdd;
+    }
+
+    public int getSsd() {
+        return ssd;
+    }
+
+    public void setSsd(int ssd) {
+        this.ssd = ssd;
+    }
+}
