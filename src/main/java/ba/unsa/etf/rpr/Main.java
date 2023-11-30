72d3a49215e50dc5f282ba71158ee2154d13e1d0
Kanita Kadusic <kkadusic2@etf.unsa.ba>
2023-11-30 23:09:16 +0100

    Main

--- src/main/java/ba/unsa/etf/rpr/Main.java
+++ src/main/java/ba/unsa/etf/rpr/Main.java
@@ -1,7 +1,29 @@
 package ba.unsa.etf.rpr;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<String> fullNames = new ArrayList<>();
+        fullNames.add("Yellow Banana");
+        fullNames.add("Green Kiwi");
+
+        FullName fullName = new FullName(fullNames);
+
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Red");
+        names.add("Orange");
+
+        ArrayList<String> surnames = new ArrayList<>();
+        surnames.add("Strawberry");
+        surnames.add("Orange");;
+
+        NameSurname nameSurname = new NameSurname(names, surnames);
+
+        Winner first = new Winner(fullName);
+        System.out.println(first.getName() + first.getSurname());
+
+        Winner second = new Winner(nameSurname);
+        System.out.println(second.getName() + second.getSurname());
     }
 }
