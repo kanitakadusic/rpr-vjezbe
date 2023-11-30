84867ff21b489ff89013a67c9af35fd6f019e7a7
Kanita Kadusic <kkadusic2@etf.unsa.ba>
2023-11-30 23:22:15 +0100

    Evaluable interface modification

--- src/main/java/ba/unsa/etf/rpr/Evaluable.java
+++ src/main/java/ba/unsa/etf/rpr/Evaluable.java
@@ -3,5 +3,7 @@ package ba.unsa.etf.rpr;
 import java.util.ArrayList;
 
 public interface Evaluable {
+    void addEvaluation(Evaluation evaluation);
+    void resetEvaluations();
     ArrayList<Evaluation> getEvaluations();
 }
