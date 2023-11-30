5c87a80cca59904e7349ad9d488ac9fc9e34e1cd
Kanita Kadusic <kkadusic2@etf.unsa.ba>
2023-11-30 23:09:09 +0100

    Winner class modification

--- src/main/java/ba/unsa/etf/rpr/Winner.java
+++ src/main/java/ba/unsa/etf/rpr/Winner.java
@@ -4,8 +4,11 @@ public class Winner {
     private final String name, surname;
     private final int numOfChars;
 
-    public Winner(FullName fullName) {
-        String longest = fullName.getLongestFullName();
+    public Winner(Object object) {
+        String longest = "";
+
+        if (object instanceof FullName) longest = ((FullName) object).getLongestFullName();
+        else if (object instanceof NameSurname) longest = ((NameSurname) object).getNameSurname(((NameSurname) object).getIndexOfLongestPair());
 
         name = longest.substring(0, longest.indexOf(' '));
         surname = longest.substring(longest.indexOf(' ') + 1);
