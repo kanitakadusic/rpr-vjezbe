59878cc4460c5951ab7b78ce22bd87b3a624eb9b
Kanita Kadusic <kkadusic2@etf.unsa.ba>
2023-11-30 16:51:54 +0100

    Pearson class

---
+++ src/main/java/ba/unsa/etf/rpr/Pearson.java
@@ -0,0 +1,26 @@
+package ba.unsa.etf.rpr;
+
+public class Pearson {
+    private String name, surname;
+
+    public Pearson(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
