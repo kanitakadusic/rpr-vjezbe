1abd27c35cac38d0fa30359ded31e844728a45f4
Kanita Kadusic <kkadusic2@etf.unsa.ba>
2023-11-30 16:59:05 +0100

    Subject class

---
+++ src/main/java/ba/unsa/etf/rpr/Subject.java
@@ -0,0 +1,25 @@
+package ba.unsa.etf.rpr;
+
+public class Subject {
+    private String name, description;
+
+    public Subject(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
